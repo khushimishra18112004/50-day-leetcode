a15a769e79a87b1fc5287053e3043ba8379c1698
Khushi Mishra <khushimishra18112004@gmail.com>
2026-07-16 22:11:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (18.34%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        # The telephone keypad mapping
+        phone_map = {
+            '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+            '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
+        }
+        
+        res = []
+        
+        def backtrack(index, path):
+            
+            if index == len(digits):
+                res.append("".join(path))
+                return
+            
+           
+            possible_letters = phone_map[digits[index]]
+            
+            for letter in possible_letters:
+                path.append(letter)          
+                backtrack(index + 1, path)   
+                path.pop()                    
+        
+        backtrack(0, [])
+        return res
