43bb06a5082ffb0579628a3f754a632ab994a588
Khushi Mishra <khushimishra18112004@gmail.com>
2026-07-19 21:53:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (90.93%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -1,12 +1,9 @@
 class Solution(object):
     def middleNode(self, head):
-        curr=head
-        count=0
-        while curr is not None:
-            count+=1
-            curr=curr.next
-        curr=head
-        for i in range(0,count//2):
-            curr=curr.next
-        return curr   
-        
+        slow=head
+        fast=head
+
+        while fast is not None and fast.next is not None:
+            slow= slow.next
+            fast=fast.next.next
+        return slow    
